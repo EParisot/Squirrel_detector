81f48c73d3cadbe65af398f6086073efd50831d7
eparisot <eparisot@student.42.fr>
2020-07-13 12:08:58 +0200

    activate cam on install

--- install.sh
+++ install.sh
@@ -8,10 +8,23 @@ python3 -c 'import sys; print("Please update your Python version to 3.5 or later
 pip3 install -r requirements.txt
 # all Done !
 echo "Dependencies Installation Done !"
+# activate camera
+if ! mountpoint -q /boot; then
+return 1
+fi
+[ -e /boot/config.txt ] || touch /boot/config.txt
+set_config_var start_x 1 /boot/config.txt
+CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
+if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
+    set_config_var gpu_mem 128 /boot/config.txt
+fi
+sed /boot/config.txt -i -e "s/^startx/#startx/"
+sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
+
 # set Wifi
 ./set_wifi.sh
 # set crontab
-echo "@reboot cd /home/pi/Squirrel_detector && sudo python3 Squirrel_detector.py &" > /etc/cron.d/sqrt_cron
+(crontab -l ; echo "@reboot cd Squirrel_detector && sudo python3 Squirrel_detector.py &") | sort - | uniq - | crontab -
 # ask for reboot
 read -p "Reboot Now (needed to restart wifi settings) ? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
