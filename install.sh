b88ffbcf2a8d3887c26e6b4fb249173957b667d3
eparisot <eparisot@student.42.fr>
2020-07-13 12:16:06 +0200

    simpler cam activation

--- install.sh
+++ install.sh
@@ -9,17 +9,7 @@ pip3 install -r requirements.txt
 # all Done !
 echo "Dependencies Installation Done !"
 # activate camera
-if ! mountpoint -q /boot; then
-return 1
-fi
-[ -e /boot/config.txt ] || touch /boot/config.txt
-set_config_var start_x 1 /boot/config.txt
-CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
-if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
-    set_config_var gpu_mem 128 /boot/config.txt
-fi
-sed /boot/config.txt -i -e "s/^startx/#startx/"
-sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
+raspi-config nonint do_camera 0
 # set Wifi
 ./set_wifi.sh
 # set crontab
