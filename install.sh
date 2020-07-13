bc8048e955eb1678c66036e00e314ec52e7a6c00
eparisot <eparisot@student.42.fr>
2020-07-13 12:19:54 +0200

    colors install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+echo -e "\e[33mInstalling Dependencies... \e[0m"
 # system dep
 apt-get update
 apt-get install python3-dev python3-setuptools python3-pip python3-picamera -y
@@ -7,11 +8,13 @@ python3 -c 'import sys; print("Please update your Python version to 3.5 or later
 # package dep
 pip3 install -r requirements.txt
 # all Done !
-echo "Dependencies Installation Done !"
+echo -e "\e[33mActivating Camera... \e[0m"
 # activate camera
 raspi-config nonint do_camera 0
+echo -e "\e[33mSet Wifi... \e[0m"
 # set Wifi
 ./set_wifi.sh
+echo -e "\e[33mAdd crontab... \e[0m"
 # set crontab
 (crontab -l ; echo "@reboot cd Squirrel_detector && sudo python3 Squirrel_detector.py &") | sort - | uniq - | crontab -
 # ask for reboot
