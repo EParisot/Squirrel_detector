aba7f1ef85a990818863a4fbe75143ab4b4ab40f
eparisot <eparisot@student.42.fr>
2020-07-13 13:04:12 +0200

    actions order install

--- install.sh
+++ install.sh
@@ -7,16 +7,15 @@ apt-get install python3-dev python3-setuptools python3-pip python3-picamera -y
 python3 -c 'import sys; print("Please update your Python version to 3.5 or later...", exit(1)) if sys.version_info.major < 3 or sys.version_info.minor < 5 else print("Python version checked")'
 # package dep
 pip3 install -r requirements.txt
-# all Done !
-echo -e "\e[33mActivating Camera... \e[0m"
 # activate camera
+echo -e "\e[33mActivating Camera... \e[0m"
 raspi-config nonint do_camera 0
-echo -e "\e[33mSet Wifi... \e[0m"
-# set Wifi
-./set_wifi.sh
-echo -e "\e[33mAdd crontab... \e[0m"
 # set crontab
+echo -e "\e[33mAdd crontab... \e[0m"
 (crontab -l -u pi ; echo "@reboot cd Squirrel_detector && sudo python3 Squirrel_detector.py &") | sort - | uniq - | crontab - -u pi
+# set Wifi
+echo -e "\e[33mSet Wifi... \e[0m"
+./set_wifi.sh
 # ask for reboot
 read -p "Reboot Now (needed to restart wifi settings) ? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
